6dddb3a8dbd04d08abaff74bdad9595bcbf78f68
duddk <duddk@scatterx.io>
2024-09-25 14:46:18 +0900

    redux-logger 사용

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
 import rootReducer from './modules/index.jsx'
-import myLogger from './middlewares/myLogger.jsx'
+// import myLogger from './middlewares/myLogger.jsx'
+import logger from 'redux-logger'
 
-const store = createStore(rootReducer, applyMiddleware(myLogger))
+// const store = createStore(rootReducer, applyMiddleware(myLogger, logger))
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
