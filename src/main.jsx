7f46ed37c76a959b5b2923e31db4ab01efd47636
duddk <duddk@scatterx.io>
2024-09-25 14:33:18 +0900

    리덕스 프로젝트 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
+import rootReducer from './modules/index.jsx'
+
+const store = createStore(rootReducer)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
