89015d109b95d941242c0170497b74d65650d320
duddk <duddk@scatterx.io>
2024-09-27 14:50:29 +0900

    proxy 사용해서 프론트 & 백엔드 서버 맞추기

--- src/api/posts.jsx
+++ src/api/posts.jsx
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 /**post 목록을 가져오는 비동기 함수 */
 export const getPosts = async () => {
-  const response = await axios.get('http://localhost:4000/posts');
+  const response = await axios.get('/api/posts');
   return response.data;
 }
 
 /**id로 포스트를 조회하는 비동기 함수 */
 export const getPostById = async id => {
-  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  const response = await axios.get(`/api/posts/${id}`);
   return response.data;
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // '/api'로 시작하는 경로를 프록시할 때 제거
+      },
+    },
+  },
+});
